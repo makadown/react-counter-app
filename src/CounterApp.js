5c242ac6171aa387937b5b0876b7bee5876cf830
makadown <maseflores79@gmail.com>
2020-11-15 15:58:12 -0600

    Mi Primer Hook

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,26 +1,29 @@
 /* NOTA: Con el plugin de VSCode se tiene un atajo para crear rapidamente el
    componente funcional:    rafcp*/
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 /** Recientemente React maneja los componentes de manera Funcional (Functional Components)
  * de modo que ya no se usan como clases.
  */
-const CounterApp = ({value}) => {
+const CounterApp = ({ value }) => {
 
+    // useState retorna un arreglo con 1 variable y 1 funcion
+    const [counter, setCounter] = useState(0);
     // handleAdd
-    const handleAdd = (e) => {
-        console.log(e)
+    const handleAdd = () => {
+        // setCounter(counter+1);
+        setCounter((c) => c + 1);
     };
 
     return (
-            <>
-                <h1>CounterApp</h1>
-                <h2> { value } </h2>
+        <>
+            <h1>CounterApp</h1>
+            <h2> {counter} </h2>
 
-                <button onClick={ handleAdd }>+1</button>
-            </>
-            );
+            <button onClick={handleAdd}>+1</button>
+        </>
+    );
 };
 
 CounterApp.propTypes = {
