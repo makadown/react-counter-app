1f473c2c57ab952809bbad34de8b7274e834a42c
makadown <maseflores79@gmail.com>
2024-04-22 11:09:35 -0700

    consolidando conceptos

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types';
 
 export const CounterApp = ( { value } ) => {
+    console.log('Rendering CounterApp :D');
     // my first Hook =)
     const [counter, setValue] = React.useState(value);
 
     const handleAdd = (event) => {
         // This is interesting... this is my very first attempt to use hooks
         console.log('handleAdd counter value before setValue', counter);
-         setValue(counter+1); // this is one way to update the state        
+        setValue(counter+1); // this is one way to update the state        
         //setValue( (c) => c + 1 ); // this is another way to update the state
 
 
@@ -16,6 +17,10 @@ export const CounterApp = ( { value } ) => {
         // Calling setValue() will not change the current counter value here...
         console.log('handleAdd counter value after setValue', counter); // counter will be the same value as before!
         // the new counter value will be shown by the useState() call in the next render.
+
+        // EVERYTIME the setValue() is called,
+        // after this method or any method that change the state value,
+        // everything in this CounterApp will be executed again.
     }
 
     const handleSubstract = () => {
@@ -23,7 +28,7 @@ export const CounterApp = ( { value } ) => {
     }
 
     const handleReset = () => {
-        setValue(0);
+        setValue(value);
     }
 
     return (
@@ -37,7 +42,7 @@ export const CounterApp = ( { value } ) => {
                 -1
             </button>
             <button onClick={ handleReset }>
-                RESET
+                RESET TO { value }
             </button>
         </>
        );
